0ce194ce7ca01548a80a715be14696e128377900
Alexandre Devos <devos.alexandre@live.fr>
2026-04-19 12:22:46 -0400

    chore: init project :tada:

---
+++ src/test/java/fr/octocorn/elasticspringboot/ElasticSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.octocorn.elasticspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
